ea3ea5523f9c6d3ab96883f296e02644cf2818ad
Aki Sasaki <aki@escapewindow.com>
2020-01-22 16:13:27 -0800

    fix poller config

--- notarization_poller/tests/data/integration_poller_config.py
+++ notarization_poller/tests/data/integration_poller_config.py
@@ -13,18 +13,20 @@ from taskcluster.aio import Queue
 def build_config(override, basedir):
     randstring = slugid.nice().lower().replace("_", "").replace("-", "")[:6]
     config = deepcopy(DEFAULT_CONFIG)
-    config.update({
-        "taskcluster_access_token": os.environ.get("TASKCLUSTER_ACCESS_TOKEN"),
-        "taskcluster_client_id": os.environ.get("TASKCLUSTER_CLIENT_ID"),
-        "provisioner_id": "test-dummy-provisioner",
-        "worker_group": "test-dummy-workers",
-        "worker_type": "dummy-worker-{}".format(randstring),
-        "worker_id": "dummy-worker-{}".format(randstring),
-        "notarization_username": "fakeuser",
-        "notarization_password": "fakepass",
-        "xcrun_cmd": (os.path.join(basedir, "fake_xcrun.py", ),
-        "verbose": True,
-    })
+    config.update(
+        {
+            "taskcluster_access_token": os.environ.get("TASKCLUSTER_ACCESS_TOKEN"),
+            "taskcluster_client_id": os.environ.get("TASKCLUSTER_CLIENT_ID"),
+            "provisioner_id": "test-dummy-provisioner",
+            "worker_group": "test-dummy-workers",
+            "worker_type": "dummy-worker-{}".format(randstring),
+            "worker_id": "dummy-worker-{}".format(randstring),
+            "notarization_username": "fakeuser",
+            "notarization_password": "fakepass",
+            "xcrun_cmd": (os.path.join(basedir, "fake_xcrun.py"),),
+            "verbose": True,
+        }
+    )
     if isinstance(override, dict):
         config.update(override)
     return config
