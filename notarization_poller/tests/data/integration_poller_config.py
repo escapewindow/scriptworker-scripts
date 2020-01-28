55424e76028c00b024a6f637c8f0de1b04270273
Aki Sasaki <aki@escapewindow.com>
2020-01-28 10:45:36 +0100

    integration poller config start

--- notarization_poller/tests/data/integration_poller_config.py
+++ notarization_poller/tests/data/integration_poller_config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import shutil
 from copy import deepcopy
 
 import slugid
@@ -9,14 +10,21 @@ from aiohttp import ClientSession
 from notarization_poller.constants import DEFAULT_CONFIG
 from taskcluster.aio import Queue
 
+FAKE_XCRUN = os.path.join(os.path.dirname(__file__), "data", "fake_xcrun.py")
+
+
+def integration_credentials():
+    # TODO support to read from local files?
+    return {
+        "taskcluster_access_token": os.environ.get("TASKCLUSTER_ACCESS_TOKEN"),
+        "taskcluster_client_id": os.environ.get("TASKCLUSTER_CLIENT_ID"),
+    }
 
 def build_config(override, basedir):
     randstring = slugid.nice().lower().replace("_", "").replace("-", "")[:6]
     config = deepcopy(DEFAULT_CONFIG)
     config.update(
         {
-            "taskcluster_access_token": os.environ.get("TASKCLUSTER_ACCESS_TOKEN"),
-            "taskcluster_client_id": os.environ.get("TASKCLUSTER_CLIENT_ID"),
             "provisioner_id": "test-dummy-provisioner",
             "worker_group": "test-dummy-workers",
             "worker_type": "dummy-worker-{}".format(randstring),
@@ -27,9 +35,24 @@ def build_config(override, basedir):
             "verbose": True,
         }
     )
+    config.update(integration_credentials())
     if isinstance(override, dict):
         config.update(override)
+    with open(os.path.join(basedir, "poller.yaml") as fh:
+        yaml.safe_dump(config, stream=fh)
     return config
+
+
+@pytest.fixture(scope="function")
+def base_work_dir(tmpdir):
+    orig_dir = os.getcwd()
+    shutil.copyfile(FAKE_XCRUN, os.path.join(str(tmpdir), "fake_xcrun.py"))
+    config = build_config({}, str(tmpdir))
+    os.chdir(str(tmpdir))
+    for i in ("log_dir", "work_dir"):
+        os.makedirs(config[i])
+    yield tmpdir
+    os.chdir(orig_dir)
 
 
 async def integration_test():
